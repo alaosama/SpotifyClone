af5d9341455f9dedeffd6a930d54ae83758ee415
alaosama <alahyaneosama1@gmail.com>
2024-05-25 11:50:58 +0200

    Spotify Clone

---
+++ src/assetss/assets.js
@@ -0,0 +1,130 @@
+import notification_icon from './notification.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import search_icon from './search.png'
+import stack_icon from './stack.png'
+import arrow_rigth_icon from './rigth.png'
+import plus_icon from './plus.png'
+import shuffle_icon from './shuffle.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import volume_icon from './volume.png'
+import zoom_icon from './zoom.png'
+import rigth_arrow_icon from './arrowr.png'
+import left_arrow_icon from './arrowl.png'
+
+
+ 
+import vie_la from './vie_la.jpg'
+import Corleone from './corleone.jpg'
+import Décu from './decu.jpg'
+import Démarre from './demarre.jpg'
+
+
+import werenoi from './vie_la_werenoi.mp3'
+import lacrim from './Lacrim_Corleone.mp3'
+import kofs from './Kofs_Décu.mp3'
+import kalashCriminel from './KalashCriminel_Démarre.mp3'
+
+
+
+export const assets = {
+    notification_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    search_icon,
+    stack_icon,
+    arrow_rigth_icon,
+    plus_icon,
+    shuffle_icon,
+    next_icon,
+    play_icon,
+    pause_icon,
+    volume_icon,
+    zoom_icon,
+    rigth_arrow_icon,
+    left_arrow_icon,
+
+
+
+
+
+}
+
+export const albumsData = [
+    {
+        id:0,
+        name: "vie_la_werenoi",
+        image: vie_la,
+        desc: "werenoi",
+        bgColor:"#2a4365"
+    },
+    {
+        id:1,
+        name: "Lacrim_Corleone",
+        image: Corleone,
+        desc: "Lacrim",
+        bgColor:"22543d"
+    },
+    {
+        id:2,
+        name: "Kofs_Décu",
+        image: Décu,
+        desc: "Kofs",
+        bgColor:"#742a2a"
+    },
+    {
+        id:3,
+        name: "KalashCriminel_Démarre",
+        image: Démarre,
+        desc: "Kalash Criminel",
+        bgColor:"#234e52"
+    },
+
+]
+
+export const songsData = [
+    {
+        id:1,
+        name: "vie_la_werenoi",
+        image: vie_la,
+        file: werenoi,
+        desc: "vie_la_werenoi",
+        duration: "2:42"
+    },
+    {
+        id:2,
+        name: "Lacrim_Corleone",
+        image: Corleone,
+        file: lacrim,
+        desc: "Lacrim_Corleone",
+        duration: "3:06"
+    },
+    {
+        id:3,
+        name: "Kofs_Décu",
+        image: Décu,
+        file: kofs,
+        desc: "Kofs_Décu",
+        duration: "4:23"
+    },
+    {
+        id:4,
+        name: "KalashCriminel_Démarre",
+        image: Démarre,
+        file: kalashCriminel,
+        desc: "KalashCriminel_Démarre",
+        duration: "3:47"
+    },
+    
+]
+
+// vie_la_werenoi,
+// Kofs_Déçu,
+// Lacrim_Corleone,
+// KalashCriminel_Démarre
